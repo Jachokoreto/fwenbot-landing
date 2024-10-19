c7e1709b29563ef7506cf26b3c4313bef8ae74af
Zer <livefatezz@gmail.com>
2024-10-19 23:48:05 +0800

    feat: minor UI tweaks

--- src/app/(landing)/components/section2/stats/Stats.tsx
+++ src/app/(landing)/components/section2/stats/Stats.tsx
@@ -21,14 +21,14 @@ const StatItem = ({
 }) => {
     const y = useTransform(
         scrollYProgress,
-        [index / 10 + 0.3, Math.min((index) / 10 + 0.4 + 0.3, 1)],
-        ["100vh", "0vh"]
+        [index / 10 + 0.3, Math.min(index / 10 + 0.4 + 0.3, 1)],
+        ['100vh', '0vh']
     )
     // const opacity
 
     return (
         <motion.div
-            className="flex h-full flex-col justify-between rounded-[24px] bg-[#fef3a1] p-6"
+            className="flex h-full flex-col justify-between rounded-[24px] border-4 border-black bg-[#fef3a1] p-6 shadow-cartoon"
             style={{ y }}
             // initial={{ y: 0 }}
             whileHover={{ scale: 1.05 }}
@@ -49,7 +49,11 @@ const statVals = [
     { title: 'Total Trade', value: '100' },
 ]
 
-const Stats = ({ scrollYProgress }: { scrollYProgress: MotionValue<number> }) => {
+const Stats = ({
+    scrollYProgress,
+}: {
+    scrollYProgress: MotionValue<number>
+}) => {
     const ref = useRef(null)
     const transform = (
         mationValue: MotionValue<number>,
@@ -64,7 +68,6 @@ const Stats = ({ scrollYProgress }: { scrollYProgress: MotionValue<number> }) =>
             ref={ref}
             className="container mx-auto flex flex-1 flex-col"
         >
-
             <div className="my-10 grid h-full w-full max-w-screen-xl auto-rows-fr grid-cols-1 items-stretch justify-around gap-6 md:grid-cols-2 md:gap-8">
                 {statVals.map((stat, index) => (
                     <StatItem
